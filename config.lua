b202502ac7b5d95fe49c1992ff5825611352eddb
Vision-Store <102265264+Vision-Store@users.noreply.github.com>
2022-03-23 23:13:14 +0000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,51 @@
+QB = {}
+
+QB.Spawns = {
+    ["legion"] = {
+        coords = vector4(195.17, -933.77, 29.7, 144.5),
+        location = "legion",
+        label = "Legion Square",
+    },
+
+    ["policedp"] = {
+        coords = vector4(428.23, -984.28, 29.76, 3.5),
+        location = "policedp",
+        label = "Police Department",
+    },
+
+    ["paleto"] = {
+        coords = vector4(80.35, 6424.12, 31.67, 45.5),
+        location = "paleto",
+        label = "Paleto Bay",
+    },
+
+    ["motel"] = {
+        coords = vector4(327.56, -205.08, 53.08, 163.5),
+        location = "motel",
+        label = "Motels",
+    },
+
+    ["airport"] = {
+        coords = vector4(-940.0, -2858.99, 13.96, 343.18),
+        location = "airport",
+        label = "LSIA",
+    },
+
+    ["sandy"] = {
+        coords = vector4(1729.65, 3714.51, 34.16, 1.54),
+        location = "sandy",
+        label = "Sandy Shores Airport",
+    },
+
+    ["busgarage"] = {
+        coords = vector4(462.53, -642.66, 28.5, 174.13),
+        location = "busgarage",
+        label = "Dash Hound Bus Garages",
+    },
+
+    ["jobcenter"] = {
+        coords = vector4(-268.23, -958.32, 31.22, 210.33),
+        location = "jobcenter",
+        label = "Job Center",
+    },
+}
